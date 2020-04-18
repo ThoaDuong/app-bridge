f3b12cd1b68d0492fc8536553e5b62861286c6a3
Thoa Duong <kimthoa2598@gmail.com>
2020-04-18 19:55:09 +0700

    fix some

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import routes from './routes';
 import ScrollToTop from './scroll/ScrollToTop';
 
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="wrapper">
-                <Router basename="/">
+                <Router basename="app-bridge/">
                     <ScrollToTop/>
                     <Switch>
                         { this.showRoute(routes) }
