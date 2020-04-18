3eab0ff26487f8f6a772ed9b364e2d17c655b7f6
Thoa Duong <kimthoa2598@gmail.com>
2020-04-18 20:20:55 +0700

    fix again

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import thunk from 'redux-thunk';
 
 const store = createStore(
     appReducers,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
