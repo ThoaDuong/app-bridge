9cee83d9092dd48b5456832dc5e37afae5f3f4ae
Thoa Duong <kimthoa2598@gmail.com>
2020-04-18 19:34:32 +0700

    Upload my react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="wrapper">
-                <Router>
+                <Router basename="/">
                     <ScrollToTop/>
                     <Switch>
                         { this.showRoute(routes) }
